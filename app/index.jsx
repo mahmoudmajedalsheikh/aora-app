330d122abcd2d9cc8fa66223ce1d530023a0859d
Eng Mahmoud Majed <mahmoudmajed404@gmail.com>
2024-10-30 17:23:55 +0300

    leave linux

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,15 @@
+import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Aora !</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Hello Aora v1.0.0!</Text>
       <StatusBar style="auto" />
+      <Link href="/home" style={{color:"blue"}}>Go To Home</Link>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
